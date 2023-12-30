7dad94fbea3cddf1886392a354fd90d69d9a0ca5
aseeralfaisal <aseeralfaisal@gmail.com>
2023-12-31 02:11:41 +0600

    api postData udpated

--- editor.script.js
+++ editor.script.js
@@ -30,7 +30,7 @@ const toastNotification = (text) => {
 
 const querySelect = (element) => document.querySelector(element);
 const querySelectAll = (element) => document.querySelectorAll(element);
-const getAttr = (element, attr) => querySelect(element).getAttribute(attr)
+const getAttr = (element, attr) => querySelect(element).getAttribute(attr);
 
 WebFont.load({
   google: {
@@ -493,13 +493,28 @@ class EditorScreen {
 
       const getFormattedBgColor = (bgColor) => {
         if (typeof bgColor === 'object') {
-          const color = bgColor.colorStops.map((i) => i.color);
-          return color.join(',');
+          const color = bgColor?.colorStops.map((i) => i.color);
+          return color?.join(',');
         }
         return bgColor;
       };
 
       if (currentCanvasData && sellerLogoInfoId) {
+        const getDropShadowValue = (element) => {
+          if (!element) return null;
+          const { blur, offsetX, offsetY } = element;
+          const result = [blur ?? 0, offsetX ?? 0, offsetY ?? 0];
+          const resultString = result.join(',');
+          
+          for(let i = 0; i < result.length; i++) {
+            if(result[i] === result[i+1]){
+              return "0"
+            }
+          }
+
+          return resultString
+        };
+
         const postData = {
           seller_logoinfo_id: sellerLogoInfoId,
           json_data: currentCanvasData,
@@ -507,19 +522,19 @@ class EditorScreen {
           logo_position: this.alignId,
           logo_backgroundcolor: bgColor === this.canvasBG ? 'transparent' : getFormattedBgColor(bgColor),
           brandName_color: logoNameElement.get('fill'),
-          brandName_fontFamely:logoNameElement.get('fontFamily'),
+          brandName_fontFamely: logoNameElement.get('fontFamily'),
           brandName_fontSize: logoNameElement.get('fontSize'),
-          brandName_letterCase: getTextCase(logoNameElement),
+          brandName_letterCase: getTextCase(logoNameElement.text),
           brandName_fontStyle: logoNameElement.get('fontStyle'),
-          brandName_letterSpace: +logoNameElement.get('charSpacing') / 10,
-          brandName_droupShadow: logoNameElement.get('shadow'),
+          brandName_letterSpace: logoNameElement.get('charSpacing') / 10,
+          brandName_droupShadow: getDropShadowValue(logoNameElement.get('shadow')),
           slogan_color: sloganNameElement.get('fill'),
-          slogan_fontFamely:sloganNameElement.get('fontFamily'),
+          slogan_fontFamely: sloganNameElement.get('fontFamily'),
           slogan_fontSize: sloganNameElement.get('fontSize'),
-          slogan_letterCase: getTextCase(sloganNameElement),
+          slogan_letterCase: getTextCase(sloganNameElement.text),
           slogan_fontStyle: sloganNameElement.get('fontStyle'),
-          slogan_letterSpace: +sloganNameElement.get('charSpacing') / 10,
-          slogan_droupShadow: sloganNameElement.get('shadow'),
+          slogan_letterSpace: sloganNameElement.get('charSpacing') / 10,
+          slogan_droupShadow: getDropShadowValue(sloganNameElement.get('shadow')),
         };
         const response = await axios.post(`https://www.mybrande.com/api/seller/logo/store`, postData);
 
@@ -551,7 +566,7 @@ class EditorScreen {
         multiplier: 1,
       };
       const savedLogo = this.canvas.toDataURL(saveSettings);
-      localStorage.clear(); 
+      localStorage.clear();
       localStorage.setItem('saved_logo', savedLogo);
       localStorage.setItem('mainEditorCounter', 3);
       location.reload();
